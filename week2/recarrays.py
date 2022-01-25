73aad4d7b31747f59ad731785a948aa5584762c9
Nikhil Patten <nikhil@ant.uwyo.edu>
2022-01-25 13:32:19 -0700

    Added some notes to my code to show my how i was unsure about the last task

--- week2/recarrays.py
+++ week2/recarrays.py
@@ -7,5 +7,5 @@ plt.ylabel("DEC")
 plt.savefig("/d/scratch/ASTR5160/week2/ravsdecplot.png")#saving figure, task 1
 for x,y in zip(objs[(objs["EXTINCTION"][...,0]>.22)]["RA",objs[(objs["EXTINCTION"][...,0]>.22)]["DEC"]):
                label = f"({x},{y})"
-               plt.annotate(label,(x,y),textcoords="offset points",xytext=(0,10),ha='center')#labeling all points with extinction >.22 in first column
+               plt.annotate(label,(x,y),textcoords="offset points",xytext=(0,10),ha='center')#labeling all points with extinction >.22 in first column, im not sure if this is right, i had some trouble figuring out how to test for loops in ipython
 plt.savefig("/d/scratch/ASTR5160/week2/labeledplot.png")#saving figure
