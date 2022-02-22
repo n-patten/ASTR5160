810e2013ed4dc6fc62b425e1bca3a035d152032f
Adam U.W. Myers <amyers14@uwyo.edu>
2022-02-22 13:27:27 -0700

    hopefully illustrative example answer for homework 1

--- homework/homework1.py
+++ homework/homework1.py
@@ -1,84 +1,110 @@
-import numpy as np
-import pandas as pd
-import argparse
-from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+# ADM this code uses a range of ideas and approaches from four different
+# ADM students in the University of Wyoming's (2022) ASTR-5160 course.
 from astropy import units as u
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy.time import Time
 from astropy.table import Table
-from astropy.time import Time # NP necessary imports
+import argparse
+import numpy as np
+from calendar import monthrange
 
-parser = argparse.ArgumentParser(description = 'Lists quasars sorted by airmass.')
-parser.add_argument('month', metavar = 'm', type = int,help = 'Takes a number, m, which corresponds to a month in the year 2022 and returns a table of quasars observable from Kitt Peak Observatory, at 11 pm MST, sorted by airmass.')
-args = parser.parse_args()
-# NP Adding argparse
+# ADM just hard-coding the offset between UTC and MST.
+utc_off = 7*u.hour
 
-def airmass(m):
-    '''Takes a number, m, which corresponds to a month in the year 2022 and returns a table of 
-        quasars observable from Kitt Peak Observatory sorted by airmass.'''
 
-    df = pd.read_csv('/d/scratch/ASTR5160/week4/HW1quasarfile.txt', header = None, names=['Coordinates'])
-    t = Table.from_pandas(df) # NP creating an astropy Table of the quasars
-    ra = []
-    dec = []
-    for i in t:
-        ra.append(i.__str__()[38:40]+'h'+i.__str__()[40:42]+'m'+i.__str__()[42:47]+'s')
-        dec.append(i.__str__()[47:50]+'d'+i.__str__()[50:52]+'m'+i.__str__()[52:56]+'s')
-        # NP running through quasar list and creating tables of their RA's and decs
-    c = SkyCoord(ra,dec,frame = 'icrs') # NP creating SkyCoords for each quasar
-    month = m # Defining month from user input
-    days = 0 # NP Number of days in the month, will be defined later
-    utcoffset = 6*u.hour # NP MST correction
-    times = [] # NP Lists that will all be utilized in the program, instantiating them now
-    altaz =[]
-    airmass = []
-    legitairmasses = []
-    indices = []
-    quasarindex = []
-    sortedquasarlist = []
-    sortedra = []
-    sorteddec = []
-    sortedtimes = []
-    sorteddates = []
-    kpno = EarthLocation.of_site('Kitt Peak National Observatory') # NP Defining KPNO
-    if (month == 2):
-        days = 28
-    elif (month == 9 or month == 4 or month == 6 or month == 11):
-        days = 30
-    else:
-        days = 31 # NP Defining the number of days depending on which month was inputted
-    for i in range(days):
-        times.append(Time('2022-'+str(month)+'-'+str(i+1)+' 23:00:00')-utcoffset)
-        # NP Creating a list of times corresponding to all the days in the month
-    for i in times:
-        altaz.append(c.transform_to(AltAz(obstime=i,location=kpno)))
-        # NP Finding Alt and Az for each quasar in the list for every day in the month
-    for i in altaz:
-        airmass.append(i.secz) # NP calculating airmass
-    for i in airmass:
-        for b in i:
-            if(b > 1):
-                legitairmasses.append(b) # NP Finding all airmasses that are above the horizon
-    sortedairmasses = np.sort(legitairmasses) # NP Sorting the airmasses
-    for i in sortedairmasses:
-        indices.append(np.where(airmass == i))
-        # NP Creating a list of indices that point to a particular quasar
-    for i in indices:
-        sortedtimes.append(i[0])
-        quasarindex.append(i[1])
-        # NP Creating a sorted list of quasars and times based on their airmasses
-    for i in quasarindex:
-        sortedquasarlist.append(t[i].__str__()[38:56])
-        sortedra.append(c[i].ra.degree)
-        sorteddec.append(c[i].dec.degree)
-    for i in sortedtimes:
-        sorteddates.append('2022-'+str(month)+'-'+str(int(i+1))+' 23:00:00')
-        # NP Creating a sorted list of dates that each quasar is observable
-    t = Table([sorteddates, sortedquasarlist, sortedra, sorteddec, sortedairmasses],
-         names = ('Date','Quasar Coordinates (hms.ss deg arcmin ")', 'RA (deg)', 'Dec (deg)', 'Airmass'))
-    # NP Defining a table that contains all of the sorted quasars and their associated information
-    return t
+def get_coords(filename):
+     """
+     Convert a .txt file containing quasars in hmsdms to a SkyCoord object.
 
-if __name__ == '__main__':
-    print(airmass(args.month))
+     Parameters
+     ----------
+     filename : Name of quasar file.
 
-# NP I know I use a lot of for loops, but I honestly cannot think of any other way
-# NP of doing this
+     Returns
+     -------
+     SkyCoord object with quasar RAs and Decs.
+     list of quasar names.
+     """
+     # ADM read in the file.
+     quasars = Table.read(filename, format='ascii.no_header')
+
+     # ADM use list comprehensions to determine the coordinates
+     ra = ["{}h{}m{}s".format(q[0:2], q[2:4], q[4:9]) for q in quasars["col1"]]
+     dec = ["{}d{}m{}s".format(q[9:12], q[12:14], q[14:18]) for q in quasars["col1"]]
+     coords = SkyCoord(ra, dec, frame='icrs')
+   
+     return coords, quasars['col1']
+
+
+def get_times(month):
+     """
+     Create astropy.time object at 6AM UTC for each day of a month.
+  
+     Parameters
+     ----------
+     month : Interger onth of the year [1-12].
+
+     Returns
+     -------
+     list of observation times (6:00 UTC).
+     """
+     # ADM monthrange returns the number of days in a given month.
+     days = np.arange(monthrange(2022, month)[1]) + 1
+
+     # ADM create a time object at 6AM UTC or 11PM MST.
+     t = ['2022-' + str(month) + '-' + str(d) + ' 23:00:00' for d in days]
+     times = Time(t, format='iso') + utc_off
+
+     return times
+
+
+def get_airmass(filename, month):
+     """
+     Lowest-airmass object in a file that can be observed on each day in a month.
+
+     Parameters
+     ----------
+     filename : Name of quasar file as a string.
+     month : Month of the year as an integer.
+
+     Returns
+     -------
+     list of observation timestamps.
+     list of hmsdms coordinates of lowest airmass quasar.
+     list of right ascension of lowest-airmass quasar
+     list of declination of lowest-airmass quasar.
+     list of airmass of lowest-airmass quasar.
+     """
+     kpno = EarthLocation.of_site("kpno")
+
+     # ADM get needed coordinates from file.
+     coords, quasars = get_coords(filename)
+     # ADM get needed time objects for passed month.
+     times = get_times(month)
+
+     # ADM all of the required quantities without appending to anything.
+     airmasses = [coords.transform_to(AltAz(obstime=t, location=kpno)).secz for t in times]
+     ii = np.array([np.argmin(np.where(am > 0, am, 1e16)) for am in airmasses])
+     airmass = np.array([np.min(np.where(am > 0, am, 1e16)) for am in airmasses])
+
+     return times, quasars[ii], coords[ii].ra.value, coords[ii].dec.value, airmass
+
+
+if __name__ == "__main__": # AMC if quasar_airmass_homework1.py is main program
+     # ADM hardcode the filename.
+     filename = "/d/scratch/ASTR5160/week4/HW1quasarfile.txt"
+
+     description = ("Return the lowest-airmass observation of any quasar listed" 
+                    "in the file".format(filename))
+
+     parser = argparse.ArgumentParser(description=description)
+     parser.add_argument('month', type=int)
+     args = parser.parse_args()
+
+     # ADM call the function to make the table and print the result
+     time, coords, ra, dec, airmass = get_airmass(filename, args.month)
+     names = ['Date (MST)', 'Quasar Coordinates', 'RA (o)', 'Dec (o)', 'Airmass']
+     t = Table([time - utc_off, coords, ra, dec, airmass], names=names)
+
+     print(t)
+
